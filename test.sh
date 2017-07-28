c72703dce62da5cfdb081b3f3292a0c33bc7d9a3
joe-at-startupmedia <joseph.persie@startupmedia.com>
2017-07-28 15:23:29 +0000

    Remove test file

--- test.sh
+++
@@ -1,9 +0,0 @@
-github-label "Startup-Media/provider-api" | grep -v priority | tr -d "#"  | gawk '
-BEGIN { FS=" - "; printf "[\n"}
-/Filesystem/ {next}
-{ printf "  {\n    \"name\": \"%s\",\n    \"color\": \"%s\"\n  },\n",
-  $2, $1
-  separator = " "
-}
-END { print "]" }
-'
